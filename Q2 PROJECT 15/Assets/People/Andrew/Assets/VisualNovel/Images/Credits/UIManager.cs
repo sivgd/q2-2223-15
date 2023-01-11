78e4dc4b336a70edb0bfd1edeb88a606d641e57c
crypticcalypte <qg1643@gmail.com>
2023-01-10 21:11:25 -0800

    > Completed Credits scene.
    I'm not linking it to Andrew's anything, due to the fact I cannot for the life of me figure out how his organization begins  to function.

---
+++ Q2 PROJECT 15/Assets/People/Andrew/Assets/VisualNovel/Images/Credits/UIManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject credit1;
+
+    public void DeleteImage()
+    {
+        Debug.Log("gone");
+    }
+}
