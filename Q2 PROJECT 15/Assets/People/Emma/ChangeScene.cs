bdad5d33095bcb885a344c2bae7a9fa4db0bee8d
crypticcalypte <qg1643@gmail.com>
2023-02-02 08:02:49 -0800

    > Making progress.
    Some buttons + more credits stuff.

--- Q2 PROJECT 15/Assets/People/Emma/ChangeScene.cs
+++ Q2 PROJECT 15/Assets/People/Emma/ChangeScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
@@ -9,6 +10,7 @@ public class ChangeScene : MonoBehaviour
     // Start is called before the first frame update
     private void OnMouseDown()
     {
+        Debug.Log("button");
         SceneManager.LoadScene(SceneToLoad);
     }
 
