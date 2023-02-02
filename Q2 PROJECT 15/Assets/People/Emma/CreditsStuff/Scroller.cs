f045e40eeb1f00474380061e0e1def054a5912bc
crypticcalypte <qg1643@gmail.com>
2023-02-02 10:10:37 -0800

    > Finished the functionality.
    I still need to make a back button art thing, but that's easy.

--- Q2 PROJECT 15/Assets/People/Emma/CreditsStuff/Scroller.cs
+++ Q2 PROJECT 15/Assets/People/Emma/CreditsStuff/Scroller.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scroller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Don’t forget using UnityEngine.UI;
+    public Slider slider;
+    public float sliderVal;
+    public GameObject p1, p2;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        sliderVal = slider.value;
+        //More efficient is to only move player when slider CHANGES
+
+        Vector3 playerPos = Vector3.Lerp(p1.transform.position, p2.transform.position, sliderVal);
+        this.transform.position = playerPos;
+
     }
+
 }
